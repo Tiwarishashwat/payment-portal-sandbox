42e6389cb5d449fcec555ff3ccddeb996a0e1f91
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2019-06-05 13:46:14 +0530

    Add files via upload

---
+++ payment-portal-sandbox/paypal/payment.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <style>
+        .bg-text 
+        {
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+        color: white;
+        font-weight: bold;
+        border: 3px solid #f1f1f1;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 2;
+        width: 80%;
+        padding: 20px;
+        text-align: center;
+        }        
+        </style>
+    </head>       
+    <body background="./wall.png">
+    <div class="bg-text">
+  <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <center><input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="6EYJYW9QTKU9W" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="./Donate.png" width="1" height="1" />
+</center>
+</form>
+</div>
+</body>
+</html>
